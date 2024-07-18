a14d26970be35c0d163f3f64f919ca8776143a48
humanbeingvish <harivishnu56614@gmail.com>
2024-07-18 20:44:26 +0400

    Day7

---
+++ Day7/Q19,20,21.ts
@@ -0,0 +1,15 @@
+let Guests: string[] = ["Albert Einstein", "Marie Curie", "Leonardo da Vinci", "Isaac Newton", "Charles Darwin"];
+console.log(`These are no of guest invited ${Guests.length}`)
+
+//question 20..
+
+let favFood:String[]=['Chappati','dosa','noodels','pancakes','happy'];
+console.log(`food that i like most are ${favFood}`);
+
+//Question 21..
+
+let book:{title:string,description:string,author:string,price:number}={
+    author:'MarkMasnson',description:"everything is hope",title:'Hope',price:21
+}
+
+console.log(`my book ${book}`);
