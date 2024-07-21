dfc73b511b4bd89418d6a12ab82a1c52e496d6a8
humanbeingvish <harivishnu56614@gmail.com>
2024-07-21 14:25:56 +0400

    Day9

---
+++ Day9/25,26,27.ts
@@ -0,0 +1,18 @@
+let alienColour: string = "green";
+
+if (alienColour === "green") {
+  console.log("Great you heard 5 points ");
+} else if (alienColour === "Yellow") {
+  console.log("you earned 10 points ");
+} else if (alienColour === "red") {
+  console.log("You earned 20 points");
+}
+
+// let alienColour1: string = "red";
+
+// if (alienColour1 === "green") {
+//   console.log("No points scored ");
+// }
+// else{
+//     console.log('You earned 10 points ')
+// }
