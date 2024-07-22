9e6b98f6a31f0882a6f119c3cec4e63a630c6048
humanbeingvish <harivishnu56614@gmail.com>
2024-07-22 21:01:27 +0400

    pending of day15

---
+++ Day15/Q43,44,45.ts
@@ -0,0 +1,11 @@
+function makeSandwich(...items:string[]){
+    return (`These are the items ordered ${items.join(", ")}.`)
+}
+
+makeSandwich("ham", "cheese");
+makeSandwich("turkey", "lettuce", "tomato");
+makeSandwich("avocado", "sprouts", "mustard", "mayo");
+
+//Q45....
+
+function Cars(...){}
