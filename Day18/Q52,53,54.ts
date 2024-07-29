a50e745aa1484a34a23d7662f25fbeed50f79c33
humanbeingvish <harivishnu56614@gmail.com>
2024-07-29 18:46:28 +0400

    Day18

--- Day18/Q52,53,54.ts
+++ Day18/Q52,53,54.ts
@@ -9,3 +9,28 @@ let SmartPhone = {
 
 
 console.log(SmartPhone);
+
+//Q53...
+
+let NestedList={
+    ProgrammingLanguage:[" Js","typeScript","Node"],
+    Framework:['Express',"Angular"],
+    tools:['Git',"Docker"]
+}
+
+let {ProgrammingLanguage,Framework,tools} = NestedList;
+    
+
+
+console.log(`Program:${ProgrammingLanguage[1]} ,framework is ${Framework[0]} and tools are ${tools}`);
+
+//Q54........
+
+function CreateObjectWithDynamicKey(key:string,val:string){
+    let KeyObject = {};
+    KeyObject[key]=val;
+    return KeyObject;
+}
+
+let Preference = CreateObjectWithDynamicKey('Vish','Glo');
+console.log(Preference);
