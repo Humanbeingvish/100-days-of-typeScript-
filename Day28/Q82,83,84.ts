a2c9b44c50bae080503b29006481814323ca73ae
humanbeingvish <harivishnu56614@gmail.com>
2024-08-09 19:39:53 +0400

    Day28

---
+++ Day28/Q82,83,84.ts
@@ -0,0 +1,25 @@
+function ReturnString(alp:string){
+return alp.length
+}
+
+console.log(ReturnString("Vishnu"));
+
+
+//Q83..
+
+function upperCaseLowercase(alp1:string){
+    let LowerCase= alp1.toLowerCase();
+    let UpperCase=alp1.toUpperCase();
+    console.log(LowerCase,UpperCase)
+}
+upperCaseLowercase('HariVishnu');
+
+//Q84...
+
+function replaceJavascriptwithTypescript(Val2:string){
+    return Val2.replace(/Javascript/gi,'typeScript')
+}
+
+console.log(replaceJavascriptwithTypescript('I love javascript and like coding in javascript'))
+
+//Q85..
