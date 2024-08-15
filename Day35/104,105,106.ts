f89d7e64e7deb990b3f05e811e39dc6a85caab1e
humanbeingvish <harivishnu56614@gmail.com>
2024-08-15 20:56:27 +0400

    'Day35'

---
+++ Day35/104,105,106.ts
@@ -0,0 +1,14 @@
+function random():boolean{
+    return Math.random() > 0.4
+}
+
+console.log(random());
+
+
+//Q105..
+
+function  dice(){
+    return Math.floor(Math.random()* 6)+1
+}
+
+console.log(dice());
