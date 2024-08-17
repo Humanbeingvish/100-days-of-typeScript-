ab38d091682b32a6327532a11d4f68596587d771
humanbeingvish <harivishnu56614@gmail.com>
2024-08-17 13:37:17 +0400

    Day40

---
+++ Day40/Q119,120,121.ts
@@ -0,0 +1,18 @@
+for (let i = 0 ; i <=10 ;i ++){
+    console.log(i)
+}
+
+let num = 1;
+
+while (num <=5) {
+    console.log(`Hello,World:${num}`);
+    num ++
+}
+
+
+
+let favMovies = ['DeadPool',"Master","The Boys",'SpiderMan']
+
+favMovies.forEach(element => {
+    console.log(`These are my fav movies ${element}`)
+});
