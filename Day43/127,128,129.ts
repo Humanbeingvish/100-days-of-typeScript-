4a1834486c16ca2144ec4e35cb25cb64fbf4e175
humanbeingvish <harivishnu56614@gmail.com>
2024-08-22 18:14:03 +0400

    Day43

--- Day43/127,128,129.ts
+++ Day43/127,128,129.ts
@@ -7,3 +7,25 @@ console.log(TraditionalFun(2,4))
 const ArrowFun = (c ,b) =>{return c * b}
 
 console.log(ArrowFun(6,7));
+
+
+////.............
+
+const multFun = (d,e,f,g) =>{
+    return d*e*f*g
+}
+
+console.log(multFun(2,2,2,2));
+
+const traditionalFun ={
+    value:"TraditionalVsArrow",
+    traditional:function (){
+        console.log(`This is from t fun: ${this.value}`)
+    },
+    ArrowFun :()=>{
+        console.log('This will not refelect ' ,this.value)
+    }
+}
+
+console.log(traditionalFun.traditional());
+console.log(traditionalFun.ArrowFun());
