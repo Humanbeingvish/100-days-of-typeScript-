f837477bab89b6c4cd97fdc63f3eac9bb9ed5dd9
humanbeingvish <harivishnu56614@gmail.com>
2024-08-25 15:48:01 +0400

    Day47

---
+++ Day47/Q139,140,141.ts
@@ -0,0 +1,21 @@
+let user = 'Dead Pool';
+
+if(user === 'Dead Pool'){
+    console.log("Executed well and vaild user is found")
+};
+
+function add(a,b){
+    return a * b
+}
+
+console.log(add(4,5));
+
+for (let i =0;i <= user.length;i ++){
+    console.log(`THE VALUE OF I IS  ${i}`)
+}
+
+////////
+async function fetchingData(){
+    const data = ''
+    console.log(data)
+}
